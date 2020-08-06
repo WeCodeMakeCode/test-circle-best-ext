9b0021a01c25038263fddcbaebcd766d23be2cc3
WeCodeMakeCode <52891037+WeCodeMakeCode@users.noreply.github.com>
2020-08-06 12:07:08 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,15 +5,14 @@ function dumpCircleData (circle2: Sprite) {
     console.logValue("filled", circle.getFilled(circle2))
 }
 let myCircleSprite: Sprite = null
-let myCircleSprite2 = circle.create(30, 2)
 for (let index = 0; index < 1; index++) {
-    myCircleSprite = circle.create(60, 2)
+    myCircleSprite = circle.createCircle(60, 2)
     dumpCircleData(myCircleSprite)
     pause(2000)
     circle.fill(myCircleSprite)
     dumpCircleData(myCircleSprite)
     pause(2000)
-    circle.setColor(myCircleSprite, 4)
+    circle.setColor(myCircleSprite, 2)
     dumpCircleData(myCircleSprite)
     pause(2000)
     circle.unfill(myCircleSprite)
